9933c2d38e0391d6450f0757e5654fa4696a1d18
VensanDrot <vensandrot@gmail.com>
2022-10-19 15:08:46 +0500

    Fix empty check

--- src/components/CardCreation/index.vue
+++ src/components/CardCreation/index.vue
@@ -61,16 +61,16 @@ export default {
       this.error_author = null;
       this.error_genre = null;
 
-      if (!this.quote.Text) {
+      if (!this.quote.Text || this.quote.Text.trim().length === 0) {
         return (this.error_text = "Text cant be empty");
       }
-      if (!this.quote.Author) {
+      if (!this.quote.Author || this.quote.Author.trim().length === 0) {
         return (this.error_author = "Author cant be empty");
       }
       if (/\d/.test(this.quote.Author)) {
         return (this.error_author = "Author line cant contain digits");
       }
-      if (!this.quote.Genre) {
+      if (!this.quote.Genre || this.quote.Genre.trim().length === 0) {
         return (this.error_genre = "Genre cant be empty");
       }
       if (/\d/.test(this.quote.Genre)) {
